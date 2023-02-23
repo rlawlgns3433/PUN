a497e7b9d6047d3accc8ff15103a363eded666ed
Kenny <rlawlgns3433@gmail.com>
2023-02-23 17:52:24 +0900

    Room Add

--- Assets/Scripts/PUN/Launcher.cs
+++ Assets/Scripts/PUN/Launcher.cs
@@ -116,6 +116,15 @@ namespace Com.MyCompany.MyGame
 
         public override void OnJoinedRoom()
         {
+            // #Critical: We only load if we are the first player, else we rely on `PhotonNetwork.AutomaticallySyncScene` to sync our instance scene.
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                Debug.Log("We load the 'Room for 1' ");
+
+                // #Critical
+                // Load the Room Level.
+                PhotonNetwork.LoadLevel("Room for 1");
+            }
             Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
         }
     }
